e7379644f981dd094a50e2fabb1eb78d70e60f7a
isabelaalbano <isabela_albano@estudante.sesisenai.org.br>
2026-04-10 12:15:26 -0300

    add

---
+++ sistema.js
@@ -0,0 +1,12 @@
+document.getElementById(formDoacao) .addEventListener("submit", function (e) {
+    e.preventDeFault();
+
+    let nome = document.getElementById("nome").value;
+    let email = document.getElementById("email").value;
+    let idade = document.getElementById("idade").value;
+    let peso = document.getElementById("peso").value;
+    let tipo_sanguineo = document.getElementById("tipo sanguineo").value;
+    let telefone = document.getElementById("telefone").value;
+    let cidade = document.getElementById("cidade").value;
+    let estado = document.getElementById("estado").value;
+}
